e2672071ba9a268ed275bd3e3fc792a698d830c0
JazzY6113 <kbogomolov22@gmail.com>
2025-03-06 09:57:00 +0700

    test except

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -22,10 +22,8 @@ class CollectTest extends TestCase
     {
         $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
 
-        // Тест для существующего ключа
         $this->assertSame(2, $collect->get('b'));
 
-        // Тест для несуществующего ключа
         $this->assertSame(['a' => 1, 'b' => 2, 'c' => 3], $collect->get('d'));
     }
 
@@ -33,15 +31,12 @@ class CollectTest extends TestCase
     {
         $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
 
-        // Тест с одним исключенным элементом
         $except = $collect->except('a');
         $this->assertSame(['b' => 2, 'c' => 3], $except->toArray());
 
-        // Тест с несколькими исключенными элементами
         $exceptMultiple = $collect->except('a', 'b');
         $this->assertSame(['c' => 3], $exceptMultiple->toArray());
 
-        // Тест с исключением нескольких элементов в виде массива
         $exceptArray = $collect->except(['a', 'c']);
         $this->assertSame(['b' => 2], $exceptArray->toArray());
     }
