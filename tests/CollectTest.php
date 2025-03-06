105affcf6a2735f7b9d96924a14f90ceb77ad43b
JazzY6113 <kbogomolov22@gmail.com>
2025-03-06 09:19:46 +0700

    test get

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -18,5 +18,16 @@ class CollectTest extends TestCase
         $this->assertSame([1, 2, 3], $values->toArray());
     }
 
+    public function testGet()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+
+        // Тест для существующего ключа
+        $this->assertSame(2, $collect->get('b'));
+
+        // Тест для несуществующего ключа
+        $this->assertSame(['a' => 1, 'b' => 2, 'c' => 3], $collect->get('d'));
+    }
+
 
 }
