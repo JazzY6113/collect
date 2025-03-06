9798374620560e25d3d0196e3d8d71b03f077a19
JazzY6113 <kbogomolov22@gmail.com>
2025-03-06 09:03:30 +0700

    test value

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -11,5 +11,12 @@ class CollectTest extends TestCase
         $this->assertSame(['a', 'b', 'c'], $keys->toArray());
     }
 
+    public function testValues()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $values = $collect->values();
+        $this->assertSame([1, 2, 3], $values->toArray());
+    }
+
 
 }
